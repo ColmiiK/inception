6722ed6fbb8df7e7adc3fd05a4679f36733514e2
Alvaro Vega <alvarvg@gmail.com>
2024-07-12 11:14:23 +0200

    databases working

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,5 +1,12 @@
 #!bin/sh
 
+echo "DB_ROOT: ${DB_ROOT}"
+echo "DB_NAME: ${DB_NAME}"
+echo "DB_USER: ${DB_USER}"
+echo "DB_USER_PASS: ${DB_USER_PASS}"
+echo "DB_ADMIN: ${DB_ADMIN}"
+echo "DB_ADMIN_PASS: ${DB_ADMIN_PASS}"
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
   chown -R mysql:mysql /var/lib/mysql
 
@@ -15,17 +22,17 @@ if [ ! -d "/var/lib/mysql/wordpress" ]; then
 USE mysql;
 FLUSH PRIVILEGES;
 DELETE FROM mysql.user WHERE User='';
-DROP DATABASE test;
+DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
-CREATE DATABASE ${DB_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_di;
+CREATE DATABASE ${DB_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASS}';
 GRANT SELECT, INSERT, UPDATE, DELETE ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 CREATE USER IF NOT EXISTS '${DB_ADMIN}'@'%' IDENTIFIED BY '${DB_ADMIN_PASS}';
 GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_ADMIN}'@'%';
 FLUSH PRIVILEGES;
 EOF
-  /usr/bin/mysql --user=mysql --bootstrap </tmp/create_db.sql
+  /usr/bin/mysql --user=mysql </tmp/create_db.sql
   rm -f /tmp/create_db.sql
 fi
