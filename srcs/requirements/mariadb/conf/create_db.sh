5e04bb34839c416cc74fdd700efdb0cde36009d3
Alvaro Vega <alvarvg@gmail.com>
2024-07-15 16:48:32 +0200

    database not connecting; env not set up

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+
+# Check if MySQL data directory exists
 if [ ! -d "/var/lib/mysql/mysql" ]; then
   chown -R mysql:mysql /var/lib/mysql
 
+  # Initialize MySQL data directory
   mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
-  tfile='mktemp'
-  if [ ! -f "$tfile" ]; then
-    return 1
-  fi
-fi
 
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
-  cat <<EOF >/tmp/create_db.sql
+  # Create a temporary file for the SQL script
+  tfile=$(mktemp)
+  if [ ! -f "$tfile" ]; then
+    echo "Failed to create temporary file"
+    exit 1
+  fi
+
+  # Generate the SQL script
+  cat <<EOF >$tfile
 USE mysql;
 FLUSH PRIVILEGES;
 DELETE FROM mysql.user WHERE User='';
@@ -22,9 +27,19 @@ CREATE DATABASE ${DB_NAME} DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_USER_PASS}';
 GRANT SELECT, INSERT, UPDATE, DELETE ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 CREATE USER IF NOT EXISTS '${DB_ADMIN}'@'%' IDENTIFIED BY '${DB_ADMIN_PASS}';
-GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_ADMIN}'@'%';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_ADMIN}'@'%';
 FLUSH PRIVILEGES;
 EOF
-  /usr/bin/mysql --user=mysql </tmp/create_db.sql
-  rm -f /tmp/create_db.sql
+
+  # Start MySQL server in the background
+  mysqld_safe --datadir=/var/lib/mysql &
+
+  # Wait for MySQL server to start (sleep for a few seconds)
+  sleep 10
+
+  # Execute the SQL script
+  /usr/bin/mysql --user=root --password=${DB_ROOT} <$tfile
+
+  # Clean up the temporary file
+  rm -f $tfile
 fi
